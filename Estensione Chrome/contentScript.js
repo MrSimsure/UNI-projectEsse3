cd36ed11ebce85cda4ba5514fcdd04ee5b15106e
Simone <benigni_simone@libero.it>
2019-11-19 12:24:42 +0100

    firt commit

---
+++ Estensione Chrome/contentScript.js
@@ -0,0 +1,25 @@
+document.body.style.backgroundColor = '#3aa757';
+
+var div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = "Hello";
+
+document.getElementById("logo").appendChild(div)
+
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+setInterval(function()
+{
+    document.body.style.backgroundColor = getRandomColor();
+},1000)
