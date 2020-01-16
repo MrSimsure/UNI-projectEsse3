83536f70547281a823137c6f8da7bacec01b38c7
SovjetPWR <37150940+SovjetPWR@users.noreply.github.com>
2020-01-16 17:06:17 +0100

    db

--- ServerPython/db.py
+++ ServerPython/db.py
@@ -1,18 +1,25 @@
 import pymysql.cursors
 
-connection = pymysql.connect(host='raspyexaequo.duckdns.org:3306',
+connection = pymysql.connect(host='raspyexaequo.duckdns.org',
                              user='projectUserSimo',
-                             password='sauropods.',
+                             password='sauropods',
                              db='projectDB',
                              charset='utf8mb4',
                              use_unicode='True',
                              cursorclass=pymysql.cursors.DictCursor)
 
 
-def getAule():
+def getAule(edificio):
+
 
     with connection.cursor() as cursor:
-        data = cursor.execute("SELECT DES FROM P09_AULE")
+
+        cursor.execute("SELECT EDIFICIO_ID FROM P09_EDIFICI WHERE DES like %s", (str(edificio)))
+        idEdificio = cursor.fetchall()
+
+
+        cursor.execute("SELECT DES FROM P09_AULE WHERE EDIFICIO_ID like %s", idEdificio[0]["EDIFICIO_ID"])
+        data = cursor.fetchall()
         connection.commit()
         cursor.close()
         return data
